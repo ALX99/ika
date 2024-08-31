b297e9e193a918d6d2764dfe4ffdf4819c96d1b3
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-08-31 16:40:10 +0900

    feat: add builtin accessLog middleware

---
+++ middleware/accessLog.go
@@ -0,0 +1,60 @@
+package middleware
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"time"
+)
+
+func init() {
+	err := Register("accessLog", &accessLog{})
+	if err != nil {
+		panic(err)
+	}
+}
+
+type accessLog struct{}
+
+func (*accessLog) Setup(_ context.Context, _ map[string]any) error {
+	return nil
+}
+
+func (*accessLog) Handle(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		st := &statusRecorder{ResponseWriter: w}
+		now := time.Now()
+		next.ServeHTTP(st, r)
+
+		slog.Info("endpoint access",
+			slog.String("method", r.Method),
+			slog.String("path", r.URL.Path),
+			slog.String("escapedPath", r.URL.EscapedPath()),
+			slog.String("remote", r.RemoteAddr),
+			slog.String("userAgent", r.UserAgent()),
+			slog.Int("status", st.status),
+			slog.Int64("duration", time.Since(now).Milliseconds()),
+		)
+	})
+}
+
+func (*accessLog) Teardown(_ context.Context) error {
+	return nil
+}
+
+type statusRecorder struct {
+	status int
+	http.ResponseWriter
+}
+
+func (w *statusRecorder) WriteHeader(statusCode int) {
+	w.status = statusCode
+	w.ResponseWriter.WriteHeader(statusCode)
+}
+
+func (w *statusRecorder) Write(b []byte) (int, error) {
+	if w.status == 0 {
+		w.status = http.StatusOK
+	}
+	return w.ResponseWriter.Write(b)
+}
