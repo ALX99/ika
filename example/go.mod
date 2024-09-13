e89a636e5e0174f8446b95661861aaada343fc30
ALX99 <46844683+ALX99@users.noreply.github.com>
2024-09-14 03:29:47 +0900

    chore: instrument with pyroscope

--- example/go.mod
+++ example/go.mod
@@ -5,9 +5,12 @@ go 1.23.0
 require (
 	github.com/alx99/ika v0.0.5
 	github.com/go-chi/chi/v5 v5.1.0
+	github.com/grafana/pyroscope-go v1.2.0
 )
 
 require (
+	github.com/grafana/pyroscope-go/godeltaprof v0.1.8 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
 	github.com/lmittmann/tint v1.0.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- example/main.go
+++ example/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"log"
+	"os"
+
 	"github.com/alx99/ika"
 	"github.com/alx99/ika/middleware"
+	"github.com/grafana/pyroscope-go"
 
 	chimw "github.com/go-chi/chi/v5/middleware"
 )
@@ -15,5 +19,23 @@ func init() {
 }
 
 func main() {
+	defer setupMonitoring()()
 	ika.Run()
+}
+
+func setupMonitoring() func() {
+	p, err := pyroscope.Start(pyroscope.Config{
+		ApplicationName:   "ika-example",
+		ServerAddress:     "https://profiles-prod-019.grafana.net",
+		BasicAuthUser:     os.Getenv("PYROSCOPE_USER"),
+		BasicAuthPassword: os.Getenv("PYROSCOPE_PASSWORD"),
+	})
+	if err != nil {
+		log.Println("failed to start pyroscope", err)
+		return func() {}
+	}
+
+	return func() {
+		p.Flush(true)
+	}
 }
