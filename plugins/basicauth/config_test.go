9a597cfb9c45a1c7ff4af8365395e4710042c09c
ALX99 <46844683+ALX99@users.noreply.github.com>
2025-02-14 12:56:58 +0900

    test: improve tests

--- plugins/basicauth/config_test.go
+++ plugins/basicauth/config_test.go
@@ -1,267 +1,166 @@
 package basicauth
 
 import (
-	"net/url"
 	"testing"
 
+	"github.com/alx99/ika"
 	"github.com/matryer/is"
 )
 
-func Test_basicAuthConfig_validate(t *testing.T) {
-	is := is.New(t)
+func TestPlugin_Setup(t *testing.T) {
 	t.Setenv("USER_ENV", "user")
 	t.Setenv("PASS_ENV", "pass")
-	type fields struct {
-		Type     string
-		Username string
-		Password string
-	}
+
 	tests := []struct {
 		name    string
-		fields  fields
+		config  map[string]any
 		wantErr bool
+		check   func(*is.I, *Plugin)
 	}{
 		{
-			name: "valid static config",
-			fields: fields{
-				Type:     "static",
-				Username: "user",
-				Password: "pass",
+			name: "valid static config with incoming",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "static",
+					"username": "user",
+					"password": "pass",
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name: "valid static config with outgoing",
+			config: map[string]any{
+				"outgoing": map[string]any{
+					"type":     "static",
+					"username": "user",
+					"password": "pass",
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name: "valid env config",
-			fields: fields{
-				Type:     "env",
-				Username: "USER_ENV",
-				Password: "PASS_ENV",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "env",
+					"username": "USER_ENV",
+					"password": "PASS_ENV",
+				},
 			},
 			wantErr: false,
 		},
 		{
 			name: "invalid type",
-			fields: fields{
-				Type:     "invalid",
-				Username: "user",
-				Password: "pass",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "invalid",
+					"username": "user",
+					"password": "pass",
+				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "missing username",
-			fields: fields{
-				Type:     "static",
-				Password: "pass",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "static",
+					"password": "pass",
+				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "missing password",
-			fields: fields{
-				Type:     "static",
-				Username: "user",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "static",
+					"username": "user",
+				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "env username not set",
-			fields: fields{
-				Type:     "env",
-				Username: "MISSING_USER_ENV",
-				Password: "PASS_ENV",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "env",
+					"username": "MISSING_USER_ENV",
+					"password": "PASS_ENV",
+				},
 			},
 			wantErr: true,
 		},
 		{
 			name: "env password not set",
-			fields: fields{
-				Type:     "env",
-				Username: "USER_ENV",
-				Password: "MISSING_PASS_ENV",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "env",
+					"username": "USER_ENV",
+					"password": "MISSING_PASS_ENV",
+				},
 			},
 			wantErr: true,
 		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &basicAuthConfig{
-				Type:     tt.fields.Type,
-				Username: tt.fields.Username,
-				Password: tt.fields.Password,
-			}
-			if err := c.validate(); !tt.wantErr {
-				is.NoErr(err)
-			} else {
-				is.True(err != nil)
-			}
-		})
-	}
-}
-
-func Test_basicAuthConfig_credentials(t *testing.T) {
-	is := is.New(t)
-	t.Setenv("USER_ENV", "user")
-	t.Setenv("PASS_ENV", "pass")
-	t.Setenv("ENCODED_USER_ENV", url.QueryEscape("user:colon"))
-	t.Setenv("ENCODED_PASS_ENV", url.QueryEscape("pass:colon"))
-
-	type fields struct {
-		Type     string
-		Username string
-		Password string
-	}
-	tests := []struct {
-		name     string
-		fields   fields
-		wantUser string
-		wantPass string
-		wantErr  bool
-	}{
 		{
-			name: "static credentials",
-			fields: fields{
-				Type:     "static",
-				Username: "user",
-				Password: "pass",
-			},
-			wantUser: "user",
-			wantPass: "pass",
-			wantErr:  false,
+			name:    "no config",
+			config:  map[string]any{},
+			wantErr: true,
 		},
 		{
-			name: "env credentials",
-			fields: fields{
-				Type:     "env",
-				Username: "USER_ENV",
-				Password: "PASS_ENV",
-			},
-			wantUser: "user",
-			wantPass: "pass",
-			wantErr:  false,
-		},
-		{
-			name: "missing env username",
-			fields: fields{
-				Type:     "env",
-				Username: "MISSING_USER_ENV",
-				Password: "PASS_ENV",
-			},
-			wantUser: "",
-			wantPass: "",
-			wantErr:  true,
-		},
-		{
-			name: "missing env password",
-			fields: fields{
-				Type:     "env",
-				Username: "USER_ENV",
-				Password: "MISSING_PASS_ENV",
-			},
-			wantUser: "",
-			wantPass: "",
-			wantErr:  true,
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			c := &basicAuthConfig{
-				Type:     tt.fields.Type,
-				Username: tt.fields.Username,
-				Password: tt.fields.Password,
-			}
-			gotUser, gotPass, err := c.credentials()
-			if !tt.wantErr {
-				is.NoErr(err)
-			} else {
-				is.True(err != nil)
-			}
-			is.Equal(gotUser, tt.wantUser)
-			is.Equal(gotPass, tt.wantPass)
-		})
-	}
-}
-
-func Test_pConfig_Validate(t *testing.T) {
-	is := is.New(t)
-	tests := []struct {
-		name    string
-		config  pConfig
-		wantErr bool
-	}{
-		{
-			name: "valid config with incoming",
-			config: pConfig{
-				Incoming: &basicAuthConfig{
-					Type:     "static",
-					Username: "user",
-					Password: "pass",
+			name: "both incoming and outgoing",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"type":     "static",
+					"username": "user",
+					"password": "pass",
+				},
+				"outgoing": map[string]any{
+					"type":     "static",
+					"username": "user",
+					"password": "pass",
 				},
 			},
 			wantErr: false,
 		},
 		{
-			name: "valid config with outgoing",
-			config: pConfig{
-				Outgoing: &basicAuthConfig{
-					Type:     "static",
-					Username: "user",
-					Password: "pass",
+			name: "default type is set to static",
+			config: map[string]any{
+				"incoming": map[string]any{
+					"username": "user",
+					"password": "pass",
+				},
+				"outgoing": map[string]any{
+					"username": "user",
+					"password": "pass",
 				},
 			},
 			wantErr: false,
-		},
-		{
-			name: "valid config with both incoming and outgoing",
-			config: pConfig{
-				Incoming: &basicAuthConfig{
-					Type:     "static",
-					Username: "user",
-					Password: "pass",
-				},
-				Outgoing: &basicAuthConfig{
-					Type:     "static",
-					Username: "user",
-					Password: "pass",
-				},
+			check: func(is *is.I, p *Plugin) {
+				// Check that credentials were set correctly, which means the default type "static" was used
+				is.Equal(string(p.inUser), "user")
+				is.Equal(string(p.inPass), "pass")
+				is.Equal(p.outUser, "user")
+				is.Equal(p.outPass, "pass")
 			},
-			wantErr: false,
-		},
-		{
-			name:    "invalid config with neither incoming nor outgoing",
-			config:  pConfig{},
-			wantErr: true,
-		},
-		{
-			name: "invalid incoming config",
-			config: pConfig{
-				Incoming: &basicAuthConfig{
-					Type:     "invalid",
-					Username: "user",
-					Password: "pass",
-				},
-			},
-			wantErr: true,
-		},
-		{
-			name: "invalid outgoing config",
-			config: pConfig{
-				Outgoing: &basicAuthConfig{
-					Type:     "invalid",
-					Username: "user",
-					Password: "pass",
-				},
-			},
-			wantErr: true,
 		},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			err := tt.config.Validate()
-			if !tt.wantErr {
-				is.NoErr(err)
-			} else {
+			t.Parallel()
+			is := is.New(t)
+			p := &Plugin{}
+			err := p.Setup(t.Context(), ika.InjectionContext{}, tt.config)
+
+			if tt.wantErr {
 				is.True(err != nil)
+			} else {
+				is.NoErr(err)
+				if tt.check != nil {
+					tt.check(is, p)
+				}
 			}
 		})
 	}
